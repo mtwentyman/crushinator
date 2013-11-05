3ccc8dc301ce667d587eef5701da421f4fb7b656
Michael Twentyman <20@thirdfloorstudios.com>
2013-11-05 15:33:02 -0500

    wrangle image url to url parameter, add logging.

--- index.js
+++ index.js
@@ -1,41 +1,60 @@
 var http        = require('http')
   , url         = require('url')
   , querystring = require('querystring')
+  , request     = require('request')
   , stream      = require('stream')
   , fs          = require('fs')
+  , bunyan      = require('bunyan')
   , gm          = require('gm');
+
+var log = bunyan.createLogger({
+  name: "myapp",
+  streams: [
+    {
+      stream: process.stderr,
+      level: "debug"
+    }
+  ]
+});
 
 server = http.createServer().listen(8124);
 
 server.on('request', function (request, response) {
 
-  var image_url = url.parse('http:/' + request.url);
+  var request_qs = querystring.parse(url.parse(request.url).query);
 
-  var options = {
-    hostname: image_url.host,
-    port: image_url.port || 80,
-    path: image_url.pathname,
-    method: 'GET'
-  };
+  if (request_qs.url) {
 
-  qs = querystring.parse(image_url.query);
+    var image_url = url.parse(request_qs.url);
 
-  var proxiedRequest = http.request(options);
-  proxiedRequest.on('response', function(proxiedResponse) {
-    console.log('STATUS: ' + proxiedResponse.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(proxiedResponse.headers));
-    response.writeHead(200, {'Content-Type': proxiedResponse.headers['content-type']});
-    var gmImg = gm(proxiedResponse);
+    var options = {
+      hostname: image_url.host,
+      port:     image_url.port || 80,
+      path:     image_url.pathname,
+      method:   'GET'
+    };
 
-    gmImg.resize(qs.w, qs.h, qs.op || '>').stream(function (err, stdout, stderr) {
-      stdout.pipe(response);
+    var proxiedRequest = http.request(options);
+
+    proxiedRequest.on('response', function(proxiedResponse) {
+      log.info({
+        "status": proxiedResponse.statusCode,
+        "headers": proxiedResponse.headers
+      });
+      response.writeHead(200, {'Content-Type': proxiedResponse.headers['content-type']});
+      var gmImg = gm(proxiedResponse);
+
+      gmImg.resize(request_qs.w || 100, request_qs.h || 100, request_qs.op || '>').stream(function (err, stdout, stderr) {
+        stdout.pipe(response);
+      });
     });
-  });
 
-  proxiedRequest.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-  });
+    proxiedRequest.on('error', function(e) {
+      log.error('problem with request: ' + e.message);
+    });
 
-  proxiedRequest.end();
+    proxiedRequest.end();
+
+  }
 
 });
